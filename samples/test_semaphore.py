9a472616aae18b208df5585e3f8233983ba7aa81
George Ramaniuk <george.ramaniuk@gmail.com>
2021-10-27 18:36:36 +0300

    fixed test_semaphore

--- samples/test_semaphore.py
+++ samples/test_semaphore.py
@@ -5,21 +5,23 @@ import pytest
 pytestmark = pytest.mark.asyncio
 
 counter = 0
-sem = None
+
 
 # TODO используя Semaphore избежать длительного сна
-async def do_request():
-    global sem, counter
+async def do_request(sem):
+    global counter
 
-    counter += 1
-    if counter > 5:
-        await asyncio.sleep(10)
-    await asyncio.sleep(0.1)
-    counter -= 1
+    async with sem:
+        counter += 1
+        if counter > 5:
+            await asyncio.sleep(10)
+        await asyncio.sleep(0.1)
+        counter -= 1
 
 
 async def test():
+    sem = asyncio.Semaphore(5)
     await asyncio.wait_for(
-        asyncio.gather(*[do_request() for _ in range(10)]),
+        asyncio.gather(*[do_request(sem) for _ in range(10)]),
         timeout=1.2,
     )
