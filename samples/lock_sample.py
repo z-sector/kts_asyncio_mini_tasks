e9c1ad22c7dc5f9daaca3b26ef9b91a1975db917
George Ramaniuk <george.ramaniuk@gmail.com>
2021-10-27 15:34:02 +0300

    fixed lock_sample

--- samples/lock_sample.py
+++ samples/lock_sample.py
@@ -1,31 +1,36 @@
 import asyncio
+from asyncio import Lock
 
 counter = 0
 
+
 # TODO используя Lock не допустить длительного сна
 # убирать sleep() или изменять время сна нельзя
-async def worker():
-    global counter
+async def worker(lock: Lock):
+    async with lock:
+        global counter
 
-    await asyncio.sleep(0.1)
-    counter += 1
-    print(counter)
-    if counter > 1:
-        await asyncio.sleep(5)
-    else:
         await asyncio.sleep(0.1)
-    counter -= 1
+        counter += 1
+        print(counter)
+        if counter > 1:
+            await asyncio.sleep(5)
+        else:
+            await asyncio.sleep(0.1)
+        counter -= 1
 
 
 res = "Успех"
 
 
 async def main():
-    global SUCCESS, res
+    global res
+
+    lock = asyncio.Lock()
 
     try:
         await asyncio.wait_for(
-            asyncio.wait([worker(), worker(), worker(), worker()]), timeout=1
+            asyncio.wait([worker(lock) for _ in range(4)]), timeout=1
         )
     except asyncio.TimeoutError:
         res = "Долгое время выполнения!"
